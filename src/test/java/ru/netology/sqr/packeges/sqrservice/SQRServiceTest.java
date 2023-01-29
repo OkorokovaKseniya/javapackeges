6f4b7ce5104db32e42ecd5aaeef8018fc64e72d1
Kseniya <okorokova.kseniya@gmail.com>
2023-01-29 23:32:15 +0300

    code reformated

--- src/test/java/ru/netology/sqr/packeges/sqrservice/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/packeges/sqrservice/SQRServiceTest.java
@@ -1,23 +1,24 @@
 package ru.netology.sqr.packeges.sqrservice;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SQRServiceTest {
     @Test
 
-    public void shouldFindExact(){
-        SQRService service= new SQRService();
+    public void shouldFindExact() {
+        SQRService service = new SQRService();
         int expected = 6;
-        int actual = service.calcSqrService(400, 200 );
-        Assertions.assertEquals(expected,actual);
+        int actual = service.calcSqrService(400, 200);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    public void shouldFindInexact(){
-        SQRService service= new SQRService();
+    public void shouldFindInexact() {
+        SQRService service = new SQRService();
         int expected = 3;
         int actual = service.calcSqrService(300, 200);
-        Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expected, actual);
     }
 
 
